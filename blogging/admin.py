9c2fe2cd2574c28a54cbf9803bf1cd549c2458d4
egpython19 <egpython19@gmail.com>
2020-05-28 19:54:09 -0700

    rebuilt entire blog repo

---
+++ blogging/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from blogging.models import Post, Category
+
+
+class CategoriesInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'modified_date'
+    inlines = [
+        CategoriesInline,
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
