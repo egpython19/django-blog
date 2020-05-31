522efc2010898d49de8cfadd358cd01e0ed599d2
egpython19 <egpython19@gmail.com>
2020-05-30 19:41:20 -0700

    Revert "add post modelform"
    This reverts commit a54e911666628970863a79daaae3297745be3b54.

--- blogging/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from blogging.models import Post
-
-
-class MyPostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'text']
--- blogging/urls.py
+++ blogging/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from blogging.views import list_view, detail_view, add_model
+from blogging.views import list_view, detail_view
 
 urlpatterns = [
     path('', list_view, name='blog_index'),
-    path('add/', add_model, name='add_post'),
     path('posts/<int:post_id>/', detail_view, name='blog_detail'),
 ]
